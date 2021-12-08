15e2226beeca890753422d736e57ab2c2a6ca793
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-07 22:38:33 -0500

    Probamos cerrar conexiones sin el defer en redis

--- models/re_model_connection.go
+++ models/re_model_connection.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	"log"
-	"sync"
 	"time"
 
 	"github.com/gomodule/redigo/redis"
@@ -16,13 +15,11 @@ var RedisCN = GetConn()
 
 func GetConn() *redis.Pool {
 
-	max := 120
-
 	p := &redis.Pool{
 		MaxIdle:         20,
-		MaxActive:       max,
-		IdleTimeout:     3 * time.Millisecond,
-		MaxConnLifetime: 10 * time.Millisecond,
+		MaxActive:       120,
+		IdleTimeout:     3 * time.Second,
+		MaxConnLifetime: 10 * time.Second,
 		Wait:            true,
 		Dial: func() (redis.Conn, error) {
 			conn, err := redis.Dial("tcp", "redis:6379")
@@ -33,47 +30,5 @@ func GetConn() *redis.Pool {
 		},
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(2 * max)
-	done := make(chan struct{})
-	for i := 0; i < max; i++ {
-		c1 := p.Get()
-		log.Println("active conn 1:", p.ActiveCount())
-		go work(done, c1, &wg)
-		c2 := p.Get()
-		log.Println("active conn 2:", p.ActiveCount())
-		go work(done, c2, &wg)
-	}
-
-	wg.Add(1)
-	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-		log.Println("requesting connection...")
-		now := time.Now()
-		for i := 0; i < max; i++ {
-			c1 := p.Get()
-			log.Println("got connection 1 took:", time.Since(now), "active:", p.ActiveCount())
-			c1.Close()
-			c2 := p.Get()
-			log.Println("got connection 2  took:", time.Since(now), "active:", p.ActiveCount())
-			c2.Close()
-		}
-	}(&wg)
-	time.Sleep(time.Second * 2)
-	close(done)
-
-	wg.Wait()
-	log.Println("active:", p.ActiveCount())
-
 	return p
-}
-
-func work(done <-chan struct{}, c redis.Conn, wg *sync.WaitGroup) {
-	log.Println("work...")
-	defer func() {
-		c.Close()
-		log.Println("work done")
-		wg.Done()
-	}()
-	<-done
 }
--- repositories/worker/re_get.go
+++ repositories/worker/re_get.go
@@ -12,10 +12,10 @@ func Re_Get_Id(idbusiness int, idcountry int) (string, error) {
 	reply, err := redis.String(models.RedisCN.Get().Do("GET", strconv.Itoa(idbusiness)+strconv.Itoa(idcountry)))
 
 	if err != nil {
-		defer models.RedisCN.Get().Close()
+		models.RedisCN.Get().Close()
 		return reply, err
 	}
 
-	defer models.RedisCN.Get().Close()
+	models.RedisCN.Get().Close()
 	return reply, nil
 }
