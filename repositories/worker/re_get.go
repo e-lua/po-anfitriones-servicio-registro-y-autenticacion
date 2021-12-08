71821352d4adb12d0d4db916a1abadf789353db2
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-07 23:07:39 -0500

    Añadiendo el comando QUIT en redis

--- repositories/worker/re_get.go
+++ repositories/worker/re_get.go
@@ -12,10 +12,10 @@ func Re_Get_Id(idbusiness int, idcountry int) (string, error) {
 	reply, err := redis.String(models.RedisCN.Get().Do("GET", strconv.Itoa(idbusiness)+strconv.Itoa(idcountry)))
 
 	if err != nil {
-		defer models.RedisCN.Close()
+		models.RedisCN.Get().Do("QUIT")
 		return reply, err
 	}
 
-	defer models.RedisCN.Close()
+	models.RedisCN.Get().Do("QUIT")
 	return reply, nil
 }
