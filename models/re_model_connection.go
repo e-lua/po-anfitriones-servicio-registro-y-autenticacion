fa36139052737c538671ad1a9573b8a1da0e19e9
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-08 10:53:55 -0500

    Conectando con redis en digital

--- models/re_model_connection.go
+++ models/re_model_connection.go
@@ -23,7 +23,7 @@ func GetConn() *redis.Pool {
 	once.Do(func() {
 		p = &redis.Pool{
 			Dial: func() (redis.Conn, error) {
-				conn, err := redis.Dial("tcp", "redis:6379")
+				conn, err := redis.Dial("tcp", "rediss://default:3eBnVvBJMUpeJdaq@po-comensales-anfitriones-do-user-10365906-0.b.db.ondigitalocean.com:25061")
 				if err != nil {
 					log.Fatal("ERROR: No se puede conectar con Redis")
 				}
