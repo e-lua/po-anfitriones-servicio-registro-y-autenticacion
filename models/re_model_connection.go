e7db8ffffbee6d09ae76f7aa096e3d49bf6e4e70
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2021-12-08 12:23:56 -0500

    Probnado conexion

--- models/re_model_connection.go
+++ models/re_model_connection.go
@@ -19,7 +19,7 @@ func GetConnRedis() *redis.Pool {
 	once.Do(func() {
 		p = &redis.Pool{
 			Dial: func() (redis.Conn, error) {
-				conn, err := redis.Dial("tcp", "default:3eBnVvBJMUpeJdaq@po-comensales-anfitriones-do-user-10365906-0.b.db.ondigitalocean.com:25061")
+				conn, err := redis.Dial("tcp", "3eBnVvBJMUpeJdaq@po-comensales-anfitriones-do-user-10365906-0.b.db.ondigitalocean.com:25061")
 				if err != nil {
 					log.Fatal("ERROR: No se puede conectar con Redis" + err.Error())
 				}
