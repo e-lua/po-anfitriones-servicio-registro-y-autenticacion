11b0e5c6dbbd0cedf6e40038ad44d1551c5e7f48
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-02-03 13:38:59 -0500

    v1.2

--- models/pg_model_connection.go
+++ models/pg_model_connection.go
@@ -24,7 +24,7 @@ func Conectar_Pg_DB() *pgxpool.Pool {
 	defer cancel()
 
 	once_pg.Do(func() {
-		urlString := "postgres://postgresx:postgresx@postgres:5432/postgresx?pool_max_conns=50"
+		urlString := "postgres://postgresx:adsfg465WFVFGdsrf3465QWFDSFGH4fsadf4fwedf@postgres:5432/postgresx?pool_max_conns=50"
 
 		config, error_connec_pg := pgxpool.ParseConfig(urlString)
 
