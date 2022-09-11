837cae715440ddfd7532ac10e3fb0ed90d477b92
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-09-11 05:19:11 -0500

    v2.54

--- services/solicitud_plan/solicitud_plan_service.go
+++ services/solicitud_plan/solicitud_plan_service.go
@@ -24,7 +24,7 @@ func Anfitriones_SendRequest_Service(idbusiness int, timezone string) (int, bool
 	params := &openapi.CreateMessageParams{}
 	params.SetTo("whatsapp:+51938488229")
 	params.SetFrom("whatsapp:+17816503313")
-	params.SetBody("Deseo el PLAN BUSINESS, mi id es: " + strconv.Itoa(idbusiness) + " - " + fecha.Format("2006-01-02 3:4:5 pm"))
+	params.SetBody("El negocio con *Id* " + strconv.Itoa(idbusiness) + " ha solicitado una inscripción al PLAN BUSINESS *el día* " + fecha.Format("2006-01-02 3:4:5 pm"))
 
 	_, err := client.ApiV2010.CreateMessage(params)
 	if err != nil {
