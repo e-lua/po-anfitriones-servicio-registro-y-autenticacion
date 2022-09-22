f95c7db991068e9534c79d30370809c2df6dde86
EdwardLopezLua <edward_lopez_cntcts@hotmail.com>
2022-09-22 02:56:14 -0500

    v2.63

--- repositories/codes/re_set_request.go
+++ repositories/codes/re_set_request.go
@@ -6,7 +6,7 @@ import (
 	models "github.com/Aphofisis/po-anfitrion-servicio-registro-y-autenticacion/models"
 )
 
-func Re_Set_Request(country int, phone int, quantity int) error {
+func Re_Set_Request(phone int, country int, quantity int) error {
 
 	_, err_do := models.RedisCN.Get().Do("SET", strconv.Itoa(phone)+strconv.Itoa(country)+"REQUEST", strconv.Itoa(quantity), "EX", 60)
 	if err_do != nil {
